82bf366e9ea41fef21192ca7cdd6d339671b84de
Georgiy <shestakov.asd@mail.ru>
2021-01-05 14:17:32 +0700

    Правка кода

--- js/tabs.js
+++ js/tabs.js
@@ -11,36 +11,6 @@ document.addEventListener('DOMContentLoaded', () => {
 				document.querySelector(`[data-tabs-path="${tabsPath}"]`).classList.add('tabs__btn--active');
 				tabsHandler(tabsPath);
 			}
-
-			if (e.target.classList.contains('tabs__arrow--prev')) {
-				let activeBtn = document.querySelector('.tabs__btn--active');
-				let activeParent = activeBtn.closest('.tabs__item');
-				let previousParent = activeParent.previousElementSibling;
-
-				if (previousParent) {
-					let prevActive = previousParent.querySelector('.tabs__btn')
-					tabsBtn.forEach(el => {el.classList.remove('tabs__btn--active')});
-					prevActive.classList.add('tabs__btn--active');
-
-					let path = prevActive.dataset.tabsPath;
-					tabsHandler(path);
-				}
-			}
-
-			if (e.target.classList.contains('tabs__arrow--next')) {
-				let activeBtn = document.querySelector('.tabs__btn--active');
-				let activeParent = activeBtn.closest('.tabs__item');
-				let nextParent = activeParent.nextElementSibling;
-
-				if (nextParent) {
-					let nextActive = nextParent.querySelector('.tabs__btn');
-					tabsBtn.forEach(el => {el.classList.remove('tabs__btn--active')});
-					nextActive.classList.add('tabs__btn--active');
-
-					let path = nextActive.dataset.tabsPath;
-					tabsHandler(path);
-				}
-			}
 		});
 	}
 
